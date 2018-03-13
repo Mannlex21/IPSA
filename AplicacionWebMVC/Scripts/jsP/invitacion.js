8a2164f06da6eca71da3b5d925c9303b05df48e4
Mannlex Murillo Macias <maalmurilloma@ittepic.edu.mx>
2018-03-13 13:04:59 -0700

    mac

--- AplicacionWebMVC/Scripts/jsP/invitacion.js
+++ AplicacionWebMVC/Scripts/jsP/invitacion.js
@@ -1,14 +1,19 @@
 ﻿$(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
-function Cotizacion(preRequisicion, departamento, ejercicio,uso,ciclo) {
+function Cotizacion(preRequisicion, requisicion, departamento, ejercicio, area, ciclo) {
+    console.log(requisicion)
+    document.getElementById('requisicionCotizacion').value = requisicion;
+    document.getElementById('departamentoCotizacion').value = departamento;
+    document.getElementById('cicloCotizacion').value = ciclo;
+    document.getElementById('areaCotizacion').value = area;
     $('#modalCotizacion').modal('show');
     $("#modalCotizacion").on('shown.bs.modal', function () {
         //$('#gridDet').jqGrid('setGridWidth', $(".modalB").width());
         $(".loaderC").fadeOut(500, function () {
             $("#divC").fadeIn(500);
             console.log("entro")
-            $("#gridDet").jqGrid().setGridParam({ postData: { preRequisicion: preRequisicion, ejercicio: ejercicio, departamento: departamento }, page: 1 }).trigger('reloadGrid');
+            //$("#gridDet").jqGrid().setGridParam({ postData: { preRequisicion: preRequisicion, ejercicio: ejercicio, departamento: departamento }, page: 1 }).trigger('reloadGrid');
         });
     }); 
     
